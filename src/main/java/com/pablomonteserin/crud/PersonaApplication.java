4ba02112be9ca1573a11f04bd280e60eb4327e11
Pablo Monteserín <pablomonteserin@pablomonteserin.com>
2023-05-21 10:05:30 +0200

    subida

--- src/main/java/com/pablomonteserin/crud/PersonaApplication.java
+++ src/main/java/com/pablomonteserin/crud/PersonaApplication.java
@@ -3,12 +3,14 @@ package com.pablomonteserin.crud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//https://www.youtube.com/watch?v=-Ih_ky5HMEA
+
 @SpringBootApplication
 public class PersonaApplication {
 
 	public static void main(String[] args) {
-		//SpringApplication.run(PersonaApplication.class, args);
-		System.out.println("hola");
+		SpringApplication.run(PersonaApplication.class, args);
+		// System.out.println("hola");
 	}
 
 }
